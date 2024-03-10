262dab6378e3df0979ada09ea702c0f4df58b888
JuampiTurner <juampiturnerlopez@gmail.com>
2024-03-10 16:42:59 -0300

    conecction to db

---
+++ connection/connection.js
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+import 'dotenv/config'
+import { database, username, password, host, dialect, port } from "../config/config.js";
+
+
+const connection = new Sequelize(database, username, password, {
+    host,
+    dialect,
+    port,
+});
+
+try {
+    await connection.authenticate();
+    console.log("Connection to DB OK")
+} catch (error) {
+    console.error("Connection error", error)
+}
+
+export default connection
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from 'express'
+import connection from "./connection/connection.js";
+import { serverPort, corsOrigin } from "./config/config.js"
+
 const app = express()
-const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Error Handler
+app.use((error, req, res, next) => {
+  const additionalData = error.additionalData || {};
+
+  const responseData = {
+    success: false,
+    message: error.message,
+    ...additionalData,
+  };
+
+  res
+    .status(error.status || 500)
+    .send(responseData);
+});
+
+let force = true
+const port = parseInt(serverPort) || 8080;
+
+connection.sync({ force })
+  .then(() => {
+    app.listen(port, () => {
+      //console.clear()
+      console.log("server OK http://localhost:" + port);
+    })
+  });
